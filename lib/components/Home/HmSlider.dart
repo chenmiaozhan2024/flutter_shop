0546c03135a82d5ed20c90c6f9ac7baba493a6dd
chenmiaozhan <1004757557@qq.com>
2026-04-28 13:49:25 +0800

    feat: 首页轮播图渲染

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -1,20 +1,35 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  HmSlider({Key? key}) : super(key: key);
+  final List<BannerItem> bannerList;
+  HmSlider({Key? key,required this.bannerList}) : super(key: key);
 
   @override
   _HmSliderState createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider(){
+    final double screenWidth=MediaQuery.of(context).size.width;
+    return CarouselSlider(items: List.generate(widget.bannerList.length, (int index){
+      return Image.network(
+        widget.bannerList[index].imgUrl,
+        fit: BoxFit.cover,
+        width: screenWidth,
+      );
+    }), options: CarouselOptions(
+      autoPlay: true,
+      height: 300,
+      viewportFraction: 1,//视口占比
+    
+     ));
+  }
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.symmetric(horizontal: 10),child: Container(
-      height: 300,
-      alignment: Alignment.center,
-      color: Colors.blue,
-       child: Text('轮播图'),
-    ),);
+    return Stack(
+      children: [_getSlider()],
+    );
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/Home/HmHot.dart';
 import 'package:hm_shop/components/Home/HmMoreList.dart';
 import 'package:hm_shop/components/Home/HmSlider.dart';
 import 'package:hm_shop/components/Home/HmSuggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -13,11 +14,16 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList=[
+    BannerItem(id: "1",imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg"), 
+    BannerItem(id: "2",imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/2.jpg"),
+    BannerItem(id: "3",imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/3.jpg")
+  ];
   List<Widget> _getScrollChildren(){
     return [
       // 轮播图
       SliverToBoxAdapter(
-        child: HmSlider(),
+        child: HmSlider(bannerList:_bannerList),
       ),
       SliverToBoxAdapter(child: SizedBox(height: 10,)),
       //分类
@@ -46,6 +52,7 @@ class _HomeViewState extends State<HomeView> {
       HmMoreList()
     ];
   }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
