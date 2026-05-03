4760bb81bb5dc3aa85e315dda6bbd2d4485fa96a
chenmiaozhan <1004757557@qq.com>
2026-05-03 19:13:47 +0800

    feat: 登录页面UI和表单校验集成

--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils{
+  static bool showLoading=false;
   static void showToast(BuildContext context,String?msg){
+    //如果已经打开了
+    if(ToastUtils.showLoading){
+      return;
+    }
+    ToastUtils.showLoading=true;
+    Future.delayed(Duration(seconds: 3),(){
+      ToastUtils.showLoading=false;
+    });
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          width: 120,
+          width: 180,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadiusGeometry.circular(40)
           ),
